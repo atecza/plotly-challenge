253eed8fb9d082ee889097d37e5c8587effe818e
atecza <agetecza@gmail.com>
2020-07-09 11:56:32 -0600

    finished homework bonus

--- static/js/app.js
+++ static/js/app.js
@@ -35,7 +35,7 @@ function buildBar(data){
       }];
     
     var barLayout = {
-        title: "Top 10 Bacteria Cultures Found",
+        title: "<b> Top 10 Bacteria Cultures Found </b>",
         margin: { t: 30, l: 90 }
     };
   
@@ -51,32 +51,60 @@ function buildBubble(mydata) {
 
     console.log(`sampleValues: ${sampleValues}`)
     console.log(`otuLabels: ${otuLabels}`)
-    var largestValue = Math.max.apply(Math, sampleValues)
 
-    //scale the sizes
+    /*scale the size of the markers
+    before I decided to use sizeref under markers I did this:
+
+    var largestValue = Math.max.apply(Math, sampleValues)
     var resizeSV = sampleValues.map(d => (d/largestValue)*130)
     console.log(`resized sample values: ${resizeSV}`)
+    */
 
 
     var trace1 = {
         x: otuID,
-        y: resizeSV,
+        y: sampleValues,
         text: otuLabels,
         mode: 'markers',
         marker: {
-            size: resizeSV,
+            size: sampleValues,
             color: otuID,
-            
+            colorscale: 'Rainbow',
+            // Sets the scale factor used to determine the rendered size of 
+            // marker points. Use with `sizemin` and `sizemode`. 
+            sizeref: .025,
+            //Has an effect only if `marker.size` is set to a numerical array. 
+            //Sets the rule for which the data in `size` is converted to pixels.
+            sizemode: 'area'
         }
     };
       
     var data = [trace1];
       
     var layout = {
-        title: 'Marker Size and Color',
+        title: '<b> Sample Values by ID </b>',
+        font: {
+            size: 18
+        },
         showlegend: false,
-        height: 500,
-        width: 1200
+        height: 600,
+        width: 1300,
+        xaxis: {
+            title: {
+              text: 'OTU ID',
+              font: {
+                size: 12,
+              }
+            }
+        },
+        yaxis: {
+            title: {
+              text: 'Sample Value',
+              font: {
+                size: 12,
+              }
+            }
+        }
     };
     
     Plotly.newPlot('bubble', data, layout);
@@ -89,16 +117,20 @@ function buildDemTable(mydata){
     var data = mydata[0]
     console.log(mydata)
 
-    var selection = d3.select('#sample-metadata').selectAll('ul').data(data);
+    var data2 = Object.entries(data)
+    console.log(data2);
 
-    
-    selection.enter()
-        .append('ul')
+
+    var div = d3.select('#sample-metadata')
+    var selection = div.selectAll('p').data(data2)
+
+    //need to style this
+    selection
+        .enter()
+        .append('p')
         .merge(selection)
-        .classed('list-group', true)
-        .html(function(d){
-        return `<li> id:${d.id} </li><li> ethnicity:${d.ethnicity} </li><li> gender:${d.gender}</li>`;
-    });
+        .text(d => `${d[0]}: ${d[1]}`)
+        .style("font-size", "9px");
 
     //removes selection at the end so not always appending
     selection.exit().remove();
@@ -110,71 +142,124 @@ function buildGauge(myData){
 
     washFreq = myData[0].wfreq
     console.log(`wash frequency: ${washFreq}`)
+    var degrees = washFreq*18, radius = .5;
 
-    var trace = {
-    type: "pie",
-    showlegend: false,
-    hole: 0.4,
-    rotation: 90,
-    values: [100 / 5, 100 / 5, 100 / 5, 100 / 5, 100 / 5, 100],
-    text: ["0-1", "2-3", "4-5", "6-7", "8-9", ""],
-    direction: "clockwise",
-    textinfo: "text",
-    textposition: "inside",
-    marker: {
-        colors: ["rgba(255, 0, 0, 0.6)", "rgba(255, 165, 0, 0.6)", "rgba(255, 255, 0, 0.6)", "rgba(144, 238, 144, 0.6)", "rgba(154, 205, 50, 0.6)", "white"]
-    },
-    labels: ["very rarely", "rarely", "moderate", "often", "very often", ""],
-    hoverinfo: "label"
-    };
-
-
-    var degrees = washFreq*9, radius = .5;
     console.log(`degrees: ${degrees}`)
     var radians = degrees * Math.PI / 180;
     var x = -1 * radius * Math.cos(radians);
     var y = radius * Math.sin(radians);
 
-    var layout = {
-        shapes:[{
-            type: 'line',
-            x0: 0,
-            y0: 0,
-            x1: x,
-            y1: y,
+    var y2 = y/2 + .5;
+    var x2 = x/2 + .5;
+
+    console.log(`x: ${x2}`)
+    console.log(`y: ${y2}`)
+
+
+    // A rotated base chart creation
+    base_chart = {
+        values: [5,5,5,5,5,5,5,5,5,5,5,45],
+        text: ["0", "", "2", "", "4","","6","","8","","10",""],
+        marker: {
+            colors:["white", "white", "white", "white", "white", "white","white","white","white","white","white","white"],
             line: {
-                color: 'black',
-                width: 8
+                width: 0
             }
-        }],
+        },
+        name: "gauge",
+        hole: .4,  
+        type: "pie",
+        direction: "clockwise",
+        rotation: 100,
+        showlegend: false,
+        textinfo: "text",
+        textposition: "outside",
+        hoverinfo: "none"
+    }
+ 
+
+    var colorWheel = {
+        values: [10, 10, 10, 10, 10, 50],
+        text: ["1", "3", "5", "7", "9", ""],
+        labels: ["very rarely", "rarely", "moderate", "often", "very often", ""],
+        marker: {
+            colors:["rgba(255, 0, 0, 0.6)", "rgba(255, 165, 0, 0.6)", "rgba(255, 255, 0, 0.6)", "rgba(144, 238, 144, 0.6)", "rgba(154, 205, 50, 0.6)", "white"],
+            line: {
+                width: 1
+            }
+        },
+        name: "gauge",
+        hole: .4,  
+        type: "pie",
+        direction: "clockwise",
+        rotation: 90,
+        showlegend: false,
+        textinfo: "text",
+        textposition: "inside",
+        hoverinfo: "label"
+    }
+
+    layout = {
+        title: {
+            text: "<b>Belly Button Wash Frequency</b><br>Scrubs per Week",
+            size: 18,
+            family: 'Courier New, bold'
+
+        },
+        xaxis: {
+            showticklabels: false,
+            showgrid: false,
+            zeroline: false,
+        },
+        yaxis: {
+            showticklabels: false,
+            showgrid: false,
+            zeroline: false,
+        },
     
-    title: 'Average Number of Washes Per Week',
-    xaxis: {visible: false, range: [-1, 1]},
-    yaxis: {visible: false, range: [-1, 1]}
-    };
-    
-    var data = [trace];
-    
-    Plotly.plot('gauge', data, layout, {staticPlot: true});
+        //Creating the dial using a line shape
+        shapes: [
+            {
+                type: 'path',
+                // triangle centered on x .5 at a height of .5
+                path: `M 0.5 0.5 L ${x2} ${y2} Z`,
+                fillcolor: 'rgba(44, 160, 101, 0.5)',
+                line: {
+                    width: 5
+                },
+                xref: 'paper',
+                yref: 'paper'
+            }
+        ],
+        height: '500',
+        width: '500',
+        
+    }
+
+    //get rid of boarders
+    base_chart['marker']['line']['width'] = 0
+    colorWheel['marker']['line']['width'] = 0
+
+    var data = [base_chart, colorWheel];
+
+    Plotly.newPlot('gauge', data, layout);
 
 }
+
 
 //put everything together
 function getData() {
     //wait for the promise 
     d3.json("data/samples.json").then((data) => {
 
-        console.log(`Data`)
-        console.log(data)
+        console.log(`Data`, data)
 
         var sampleData = data.samples
-        console.log(`sample Data`)
-        console.log(sampleData)
+        console.log(`sample Data`, sampleData)
 
         var metaData = data.metadata
         //myData is an array of dictionaries. One for each person in the study
-        console.log(`meta Data`)
-        console.log(metaData)
+        console.log(`meta Data`, metaData)
         
         //get the unique ids into an array
         var subjectIDs = []
@@ -197,11 +282,9 @@ function getData() {
         //allowed it to look for number
         var selectedMeta = metaData.filter(s => s.id === parseInt(selectedID));
         
-        console.log(`selectedData`)
-        console.log(selectedData)
+        console.log(`selectedData`, selectedData)
 
-        console.log('selectedMeta')
-        console.log(selectedMeta)
+        console.log('selectedMeta', selectedMeta)
 
         //build the bar graph out of the selected dictionary
         buildBar(selectedData)
--- static/js/test.js
+++ static/js/test.js
@@ -17,3 +17,57 @@ sampleData.forEach(d => {
 
   // clear the input value
   d3.select("#selDataset").node().value = "";
+
+
+
+  function buildGauge(myData){
+
+    washFreq = myData[0].wfreq
+    console.log(`wash frequency: ${washFreq}`)
+
+    var trace = {
+    type: "pie",
+    showlegend: false,
+    hole: 0.4,
+    rotation: 90,
+    values: [100 / 5, 100 / 5, 100 / 5, 100 / 5, 100 / 5, 100],
+    text: ["0-1", "2-3", "4-5", "6-7", "8-9", ""],
+    direction: "clockwise",
+    textinfo: "text",
+    textposition: "inside",
+    marker: {
+        colors: ["rgba(255, 0, 0, 0.6)", "rgba(255, 165, 0, 0.6)", "rgba(255, 255, 0, 0.6)", "rgba(144, 238, 144, 0.6)", "rgba(154, 205, 50, 0.6)", "white"]
+    },
+    labels: ["very rarely", "rarely", "moderate", "often", "very often", ""],
+    hoverinfo: "label"
+    };
+
+
+    var degrees = washFreq*9, radius = .5;
+    console.log(`degrees: ${degrees}`)
+    var radians = degrees * Math.PI / 180;
+    var x = -1 * radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+
+    console.log(`x: ${x}`)
+
+    var trace2 = {
+        type: 'scatter',
+        
+
+    }
+
+    var layout = {
+        title: 'Average Number of Washes Per Week',
+        xaxis: {visible: false, range: [-1, 1]},
+        yaxis: {visible: false, range: [-1, 1]}
+    };
+
+    
+    var data = [trace, trace2];
+    
+    Plotly.plot('gauge', data, layout, {staticPlot: true});
+
+}
+
+domain: {"x": [0, .48]},
